64e239f62b61f8aa2fba6df4d38bd150e1cbdc73
kekich <patoawach3o@gmail.com>
2023-09-03 10:53:49 +0300

    я хз лол

--- first test/src/Main.java
+++ first test/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int [] array = {1,2,3,10,10,10,10,10,50};
+        int [] array = {1,2,3,10,10,10,10,10,5};
         float total = 0;
         float avg = 0;
         float sum = 0;
