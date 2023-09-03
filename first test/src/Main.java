09c2b080bb69775f2c0ad2856805f285725daffa
kekich <patoawach3o@gmail.com>
2023-09-03 10:49:09 +0300

    so first наход

---
+++ first test/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,10,10,10,10,10,10};
+        float total = 0;
+        float avg = 0;
+        float sum = 0;
+        for (int i = 0; i < array.length; i++){
+            total = sum += array[i];
+        }
+        avg = total/ array.length;
+        System.out.println(avg);
+    }
+}
